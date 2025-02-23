10f491f672dcba65b6cc0f7a52cc3e333c8f5a5b
Monal Raj <40452966+monal9798@users.noreply.github.com>
2025-02-23 12:15:57 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,11 @@
+/*Basic Program of Java To Print Hello World
+
+To Complie Code write code javac Filename.java
+To Run java Filename
+*/
+
+class Program1{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
